165d7f1c1c9029b8c11c957ac01f2f109f532508
chiheb <chihababiza9@gmail.com>
2025-01-25 20:00:17 +0100

    startiing code

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 import dotenv from 'dotenv';
 dotenv.config();
+import userRoutes from './routes/userRoutes.js';
 
 // Set view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', userRoutes);
 
 
 app.listen(PORT, () => {
